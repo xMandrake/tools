915df4dbb2e1a92d13af0b88c52c860307d30b3a
xDrake <anton.kochurin@gmail.com>
2015-08-22 12:56:01 +0300

    save

---
+++ src/main/java/ua/lg/dev/utils/Main.java
@@ -0,0 +1,10 @@
+package ua.lg.dev.utils;
+
+/**
+ * Created by xDrake on 22.08.15.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
